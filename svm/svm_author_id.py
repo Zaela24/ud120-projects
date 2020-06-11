d9d8073b5489c9155dd3622c7c122071477193b5
Zaela24 <zaela.marie.rose@gmail.com>
2020-06-11 16:59:12 -0500

    Finished mini-project 2 (SVM)

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,11 +7,13 @@
     Sara has label 0
     Chris has label 1
 """
-    
+import collections
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -20,11 +22,31 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+clf = svm.SVC(kernel="rbf", C=10000.)
+
+# features_train = features_train[:len(features_train)//100]
+# labels_train = labels_train[:len(labels_train)//100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), 's')
+
+t1 = time()
+pred = clf.predict(features_test)
+print("prediction time:", round(time()-t1, 3), 's')
 
 
-#########################################################
-### your code goes here ###
+accuracy = accuracy_score(labels_test, pred)
 
-#########################################################
+#print(pred)
+print(accuracy)
+
+# answer = [pred[10], pred[26], pred[50]]
+# for num in answer:
+#     print(num)
+
+pred_count = collections.Counter(pred)
+print(pred_count)
+
 
 
