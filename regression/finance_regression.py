696590d5e35f5ce08f5bd231c383d2ef1cf9e0a7
Zaela24 <zaela.marie.rose@gmail.com>
2020-06-12 18:49:42 -0500

    Finished mini-project 4 (linear regression)
    Coverted some lines to work in Python 3

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -15,21 +15,25 @@
 
 import sys
 import pickle
-sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+from tools.feature_format import featureFormat, targetFeatureSplit
+from sklearn.linear_model import LinearRegression
+
+dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "rb") )
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+features_list = ["bonus", "long_term_incentive"]
+data = featureFormat( dictionary, features_list,
+                      remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
+
+
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,7 +42,10 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
+print(reg.coef_, reg.intercept_, reg.score(feature_test, target_test))
 
 
 
