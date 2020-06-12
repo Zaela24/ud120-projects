0635cc0d875b6e07bf44db41bea036e26e23165c
Zaela24 <zaela.marie.rose@gmail.com>
2020-06-12 02:16:48 -0500

    Finished mini-project 3 (Choose your own algorithm)

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -19,13 +21,16 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), 's')
 
+t1 = time()
+pred = clf.predict(features_test)
+print("prediction time:", round(time()-t1, 3), 's')
 
-#########################################################
-### your code goes here ###
+accuracy = accuracy_score(labels_test, pred)
 
-
-#########################################################
-
-
+print(accuracy)
